6bf605617e232c4d1510be7976367060e4e3d48a
france <frongrider2@gmail.com>
2024-05-21 16:08:08 +0700

    ts boilerplate

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+// import { http, createPublicClient } from 'viem';
+// import { mainnet } from 'viem/chains';
+
+// export const publicClient = createPublicClient({
+//   chain: mainnet,
+//   transport: http(),
+// });
+
+// const main = async () => {
+//   const blockNumber = await publicClient.getBlockNumber();
+//   console.log(blockNumber);
+// };
+
+// main();
+
+console.log('Hello World!');
